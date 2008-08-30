b3519b83a16290b16960824460a371f84832e325
Keiji, Yoshimi <walf443@gmail.com>
2008-08-30 12:35:27 +0900

    added doc.

--- lib/rack/request/jpmobile.rb
+++ lib/rack/request/jpmobile.rb
@@ -1,5 +1,24 @@
 module Rack
   class Request
+    # SYNOPSIS
+    #
+    #   require 'rack/jpmobile'
+    #   class YourApp
+    #     def call env
+    #       req = Rack::Request::Jpmobile.new env
+    #       if req.mobile?
+    #         p req.mobile   #=> Jpmobile::Mobile::xxxx's instance ( for example, Jpmobile::Mobile::Docomo ).
+    #         return [ 403, {'Content-Type' => 'text/plain'}, '403 FORBIDDEN'] unless req.mobile.valid_ip?
+    #
+    #         # do something for mobile
+    #       else
+    #         # do something for pc
+    #       end
+    #     end
+    #   end
+    #
+    # SEE ALSO: +Jpmobile::RequestWithMobile+, +Jpmobile::Mobile::AbstractMobile+
+    #
     class Jpmobile < ::Rack::Request
       include ::Jpmobile::RequestWithMobile
     end
